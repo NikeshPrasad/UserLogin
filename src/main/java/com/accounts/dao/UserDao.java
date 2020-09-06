8c716e1882c77753c1e9640bfa7842a0a1cd751e
nikesh <nikeshprasad9@gmail.com>
2020-09-06 23:03:42 +0530

    changed implementation from CrudRepository to JpaRepository

--- src/main/java/com/accounts/dao/UserDao.java
+++ src/main/java/com/accounts/dao/UserDao.java
@@ -1,9 +1,9 @@
 package com.accounts.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.accounts.models.User;
 
-public interface UserDao extends CrudRepository<User, String>{
+public interface UserDao extends JpaRepository<User, String>{
 
 }
